38d7d0bc4052cd96cf8cdb1032812a13b0d8cbdd
deniseuan <denis.euan@udem.edu>
2022-03-13 05:51:19 -0600

    Update tarjeta producto

--- energ-1.0/src/components/TarjetaProducto.js
+++ energ-1.0/src/components/TarjetaProducto.js
@@ -3,28 +3,29 @@ import { Button, Card, Container, Form, Row, Col } from 'react-bootstrap';
 import img1 from '../assets/img/1.png'
 
 
+
 export default class TarjetaProducto extends Component {
   render() {
     return (
       <div>
-          <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={img1} style={{ width: '4rem'}}/>
+          <Card className="mt-5" style={{ width: '18rem' }}>
+            <Card.Img className="mt-2 mx-auto" variant="top" src={img1} style={{ width: '4rem'}}/>
             <Card.Body>
                 <Card.Title>Card Title</Card.Title>
                 <Card.Text>
                 Some quick example text to build on the card title and make up the bulk of
                 the card's content.
                 </Card.Text>
-                <Container style={{align: 'center'}}>
+                <Container className="" style={{align: 'center'}}>
                   <Row>
-                    <Col>
-                    <Button variant="primary">-</Button>
+                    <Col className="d-flex align-items-center">
+                      <div >
+                        <p><strong>$ 99.00</strong></p>
+                      </div>
+                      
                     </Col>
                     <Col>
-                    <Form.Control type="number" placeholder="1" />
-                    </Col>
-                    <Col>
-                    <Button variant="primary">+</Button>
+                    <Button variant="danger">Ver producto</Button>
                     </Col>
                   </Row>
                 </Container>
