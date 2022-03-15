0e7c47307188818d0c50ee13e8f94c21b5ec799c
deniseuan <denis.euan@udem.edu>
2022-03-14 21:54:19 -0600

    Update DetalleProductos.js

--- energ-1.0/src/pages/DetalleProductos.js
+++ energ-1.0/src/pages/DetalleProductos.js
@@ -15,8 +15,7 @@ const DetalleProductos = () => {
           <h2>Eso es todo amigos</h2>
           <h2>Esto fue antes del merge</h2>
           <h3>Prueba pull request</h3>
-
-        <h3>Cambio realizado en main</h3>
+          <h3>Cambio realizado en main</h3>
 
 
         <Footer/>
