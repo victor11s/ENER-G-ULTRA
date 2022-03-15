4010d275bdf9999c501d2c6fd843beed5398ef59
deniseuan <denis.euan@udem.edu>
2022-03-14 21:47:41 -0600

    Update DetalleProductos.js

--- energ-1.0/src/pages/DetalleProductos.js
+++ energ-1.0/src/pages/DetalleProductos.js
@@ -10,10 +10,10 @@ const DetalleProductos = () => {
 
           <h1>Aqui van los productos</h1>
           <h2>Eso es todo amigos</h2>
-
-        <h2>Eso es todo amigos</h2>
-        <h2>Esto fue antes del merge</h2>
-        <h3>Prueba pull request</h3>
+          <h1>Cambio en branch 1 antes de merge main</h1>
+          <h2>Eso es todo amigos</h2>
+          <h2>Esto fue antes del merge</h2>
+          <h3>Prueba pull request</h3>
 
         <Footer/>
     </div>
