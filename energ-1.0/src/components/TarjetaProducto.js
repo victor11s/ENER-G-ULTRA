49607d9c0711a9b677a738ac996eae40d087716d
deniseuan <denis.euan@udem.edu>
2022-03-15 20:39:41 -0600

    actualizacion detalle producto

--- energ-1.0/src/components/TarjetaProducto.js
+++ energ-1.0/src/components/TarjetaProducto.js
@@ -1,43 +1,41 @@
 import React, { Component } from 'react'
 import { Button, Card, Container, Form, Row, Col } from 'react-bootstrap';
 import img1 from '../assets/img/1.png'
-import {NavLink} from './NavComponent'
+import { NavLink } from './NavComponent'
 
 
-function TarjetaProducto (props) {
-    return (
-      <div>
-         <Container className="" style={{align: 'center'}}>
-           <Row>
-            <Col>
+function TarjetaProducto(props) {
+  return (
+    <div>
+      <Container className="" style={{ align: 'center' }}>
+        <Row>
+          <Col>
             <Card className="mt-5 p-2" style={{ width: '18rem' }}>
-            <Card.Img className="mt-2 mx-auto" variant="top" src={img1} style={{ width: '4rem'}}/>
-            <Card.Body>
+              <Card.Img className="mt-2 mx-auto" variant="top" src={img1} style={{ width: '4rem' }} />
+              <Card.Body>
                 <Card.Title>{props.nombre}</Card.Title>
                 <Card.Text>
                   {props.descripcion}
                 </Card.Text>
                 {/* <Container className="" style={{align: 'center'}}> */}
-                  <Row>
-                    <Col className="d-flex align-items-center">
-                      <div >
-                        <p><strong>MXN$ {props.precio}</strong></p>
-                      </div>
-                    </Col>
-                    <Col>
-
+                <Row>
+                  <Col className="d-flex align-items-center">
+                    <div >
+                      <p><strong>MXN$ {props.precio}</strong></p>
+                    </div>
+                  </Col>
+                  <Col>
                     <Button variant="danger"><NavLink to='/detatalleProducto'>Ver producto</NavLink></Button>
-
-                    </Col>
-                  </Row>
+                  </Col>
+                </Row>
                 {/* </Container> */}
-            </Card.Body>
-        </Card>
-        </Col>
+              </Card.Body>
+            </Card>
+          </Col>
         </Row>
-        </Container>
-        </div>
-    )
+      </Container>
+    </div>
+  )
 }
 
 export default TarjetaProducto;
