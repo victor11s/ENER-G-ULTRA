4ec8d55b87a187fa7bd37a9d1725562556208d6b
deniseuan <denis.euan@udem.edu>
2022-03-20 23:17:18 -0600

    conexion de detalle producto a base datos

--- energ-1.0/src/components/TarjetaDetalleProducto.js
+++ energ-1.0/src/components/TarjetaDetalleProducto.js
@@ -10,21 +10,37 @@ function TarjetaDetalleProducto() {
 
     let { id, nombre } = useParams();
 
-    const [producto, setProducto]=useState([]);
+    const [producto, setProducto] = useState([]);
+    const [ingredientes, setIngredientes] = useState([]);
 
-    async function getProducto(){
-        console.log(id);
-        await Axios.get('http://localhost:3001/api/getProducto',{ params: {idProducto: id,} }).then((response) => {
-            // console.log(response.data);
-            setProducto(response.data[0]);
+    async function getProducto() {
 
-        })
     }
+
+
     useEffect(() => {
-        getProducto();
+        const axiosGet = async () => {
+            console.log(id);
+            await Axios.get('http://localhost:3001/api/getProducto',
+                {
+                    params: {
+                        idProducto: id,
+                    }
+                }).then((response) => {
+                    // console.log(response.data);
+                    setProducto(response.data[0]);
+                    let ing = response.data[0].ingredientes;
+                    let ings = ing.split('\,');
+                    setIngredientes(ings);
+                });
+        }
+        axiosGet();
     }, []);
 
     console.log(producto);
+    console.log(ingredientes);
+
+    // console.log(ingredientes);
 
     return (
         <div>
@@ -41,7 +57,7 @@ function TarjetaDetalleProducto() {
                             <Row>
                                 <p className='mr-6'>{producto.descripcion}</p>
                             </Row>
-                            <h2 className='text-danger'>MXN$20</h2>
+                            <h2 className='text-danger'>MXN ${producto.precio}</h2>
                             <Form className='mt-3'>
                                 <Row>
                                     <Col className='sm-6 md-2 lg-2' >
@@ -57,26 +73,22 @@ function TarjetaDetalleProducto() {
                                 <Table responsive="sm">
                                     <thead>
                                         <tr>
-                                            <th className='justify-content-md-center'>LOREM IPSUM</th>
+                                            <th className='justify-content-md-center'>Tu bebida {nombre} contiene: </th>
                                             <th ></th>
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        <tr>
-                                            <td>LOREM IPSUM</td>
-                                            <td>-</td>
-                                            <td>-</td>
-                                        </tr>
-                                        <tr>
-                                            <td>LOREM IPSUM</td>
-                                            <td>-</td>
-                                            <td>-</td>
-                                        </tr>
-                                        <tr>
-                                            <td>LOREM IPSUM</td>
-                                            <td>-</td>
-                                            <td>-</td>
-                                        </tr>
+
+                                        {
+                                            ingredientes.map(ingrediente => {
+                                            let ingM = ingrediente.charAt(0).toUpperCase()+ingrediente.slice(1);
+                                                return (
+                                                    <tr key={ingrediente.toString() + 'a'}>
+                                                        <td key={ingrediente.toString() + 'b'}>{ingM}</td>
+                                                    </tr>
+                                                )
+                                            })
+                                        }
                                     </tbody>
                                 </Table>
                             </Row>
