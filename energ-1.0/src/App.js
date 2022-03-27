8ff4eecd7d045639020561ff281e0541f8ead02d
deniseuan <denis.euan@udem.edu>
2022-03-26 19:55:45 -0600

    Update App.js

--- energ-1.0/src/App.js
+++ energ-1.0/src/App.js
@@ -19,7 +19,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<LandingPage />} /> 
         <Route path="/catalogo" element={<Catalogo />} />
-        <Route path="/detalleProducto/:id" element={<DetalleProductos />} />
+        <Route path="/detalleProducto/:id/:nombre" element={<DetalleProductos />} />
         <Route path="/admiAgregarProducto/" element={<AdmiAgregarProducto />} />
         
 
