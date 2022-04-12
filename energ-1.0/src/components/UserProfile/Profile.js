89593393b306076e5273d796095954c4afc78280
victor11s <vic.manuel35@gmail.com>
2022-04-12 18:52:28 -0500

    Creacion de Menu de Perfil y Componentes

--- energ-1.0/src/components/UserProfile/Profile.js
+++ energ-1.0/src/components/UserProfile/Profile.js
@@ -4,18 +4,41 @@ import React from 'react'
 import NavBar from '../NavBar';
 import Footer from '../Footer';
 
+import { Button, Card, Col, Container, Form, Row, Table } from 'react-bootstrap'
+
 
 
 function Profile() {
-  return (
-    <>
+    return (
+        <>
 
-    <NavBar/>
+            <NavBar />
 
-    <Footer/>
+            <Container>
+                <Row>
+                    <Col>
+                        <Button variant="danger">Información Personal</Button>
+                    </Col>
+
+                    <Col>
+                        <Button variant="danger">Direcciones</Button>
+                    </Col>
+
+                    <Col>
+                        <Button variant="danger">Mis Pedidos</Button>
+                    </Col>
+                </Row>
 
 
-    </>
-  )
+            
+            </Container>
+
+
+
+            <Footer />
+
+
+        </>
+    )
 }
 export default Profile;
